1c70a98b3e3f20982f9b0cde2abf7e02020577f1
Max Medina <maxmedina05@gmail.com>
2017-10-21 21:48:05 -0400

    add product endpoints close #1

--- modules/product/product.module.js
+++ modules/product/product.module.js
@@ -3,6 +3,7 @@ const router                            = express.Router();
 const productController            = require('./product.controller');
 
 router.route('/').get(productController.getAll);
+router.route('/').delete(productController.removeAll);
 router.route('/').post(productController.addOne);
 router.route('/:objectId').get(productController.getOne);
 router.route('/:objectId').put(productController.updateOne);
